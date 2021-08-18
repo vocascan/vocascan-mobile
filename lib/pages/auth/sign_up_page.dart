b364405f8338daafdcdea3807771e1413eb9f9d0
Florian Sabonchi <sabonchi@posteo.de>
2021-08-18 19:27:58 +0200

    updated sing up page added a illustration

--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:vocascan_mobile/pages/widgets/rounded_button.dart';
 import 'package:vocascan_mobile/pages/widgets/rounded_input_field.dart';
+import 'package:vocascan_mobile/pages/widgets/text_field_container.dart';
 
 class SignUpPage extends StatefulWidget{
   @override
@@ -9,41 +12,49 @@ class SignUpPage extends StatefulWidget{
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          children:  <Widget>[
-            Flexible(
-              flex: 6,
-              fit: FlexFit.tight,
-              child:  SvgPicture.asset(
-              "assets/images/graphics/undraw_social_thinking.svg",
-              height: size.height * 0.25,
-              width: size.height * 0.01,
-            ),),
-            Flexible(flex: 2,
-              child:  RoundedInputField(
-              hintText: 'Password',
-              icon: Icons.password,
-              obscureText: true,
-              onChanged: (String value) {},
-            ),),
-            Flexible(flex: 2,
-              child:  RoundedInputField(
-              hintText: 'Confirm Password',
-              icon: Icons.password,
-              obscureText: true,
-              onChanged: (String value) {
+        body:  Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            children:  <Widget>[
+              SingleChildScrollView(child: Flexible(
+                  flex: 4,
+                  child: Flex(direction: Axis.vertical, children: <Widget>[
+                    SvgPicture.asset(
+                      "assets/images/graphics/undraw_social_thinking.svg",
+                      height: size.height * 0.3,
+                      width: size.height * 0.25,
+                    ),
+                    TextFieldContainer(child: Text("Try thinking up 3 or more random words you'll easily remember"),
+                      decoration: BoxDecoration(),)
+                  ],)) ,),
+              Flexible(flex: 2,
+                child:  RoundedInputField(
+                  hintText: 'Password',
+                  icon: Icons.password,
+                  obscureText: true,
+                  onChanged: (String value) {},
+                ),),
+              Flexible(flex: 2,
+                child:  RoundedInputField(
+                  hintText: 'Confirm Password',
+                  icon: Icons.password,
+                  obscureText: true,
+                  onChanged: (String value) {
 
-              },
-            ),)
-          ],
+                  },
+                ),),
+              Flexible(flex: 2,
+                  child: RoundedButton(text: 'Finish', press: () {
+
+                  },))
+            ],
+          ),
         ),
-      ),
     );
   }
 
