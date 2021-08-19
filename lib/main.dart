db885a8c0ca77af3d4c2e2dd90790fcf7cb10170
Florian Sabonchi <sabonchi@posteo.de>
2021-08-19 17:36:15 +0200

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vocascan_mobile/pages/home/home_page.dart';
 import 'package:vocascan_mobile/pages/auth/login_page.dart';
 
 void main() {
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
