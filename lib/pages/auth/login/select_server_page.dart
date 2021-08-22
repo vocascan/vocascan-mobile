8707a85e2c885508545fda10aa85c5aae19adbe8
Florian Sabonchi <sabonchi@posteo.de>
2021-08-22 21:43:35 +0200

    if domain is not valid then the continue button is disabled

--- lib/pages/auth/login/select_server_page.dart
+++ lib/pages/auth/login/select_server_page.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -5,15 +8,23 @@ import 'package:flutter_svg/svg.dart';
 import 'package:vocascan_mobile/pages/widgets/rounded_button.dart';
 import 'package:vocascan_mobile/pages/widgets/rounded_input_field.dart';
 import 'package:vocascan_mobile/pages/widgets/text_field_container.dart';
+import 'package:http/http.dart';
 
-class SelectServerPage extends StatelessWidget{
+class SelectServerPage extends StatefulWidget{
   final PageController controller;
   SelectServerPage({required this.controller});
 
   @override
+  _SelectServerPageState createState() => _SelectServerPageState();
+}
+
+class _SelectServerPageState extends State<SelectServerPage> {
+  bool _serverValid = false;
+  TextEditingController _serverUrl = new TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    TextEditingController serverUrl = new TextEditingController();
 
 
     return Scaffold(body: Center(
@@ -30,14 +41,31 @@ class SelectServerPage extends StatelessWidget{
                 decoration: BoxDecoration()
               ),
               RoundedInputField(
-                controller: serverUrl,
-                onChanged: (String value) {  },
-                hintText: 'Server',
+                controller: _serverUrl,
+                onChanged: (String serverUrl) async{
+                  try{
+                    final response = await get(Uri.parse("$serverUrl/api/info"));
+                    setState(() {
+                      if (response.statusCode == 200) {
+                        final jsonResult = json.decode(response.body);
+                        _serverValid = jsonResult.containsKey('identifier');
+                      } else {
+                        _serverValid = false;
+                      }
+                    });
+                  }
+                  catch(_){
+                    setState(() {
+                      _serverValid = false;
+                    });
+                  }
+                },
+                hintText: 'https://domain.com',
                 icon: Icons.cloud,
               ),
-              RoundedButton(text: 'Continue',
+              RoundedButton(disabled: !_serverValid, text: 'Continue',
                 press: () {
-                  controller.animateToPage(controller.page!.toInt() + 1,
+                return !_serverValid? null: widget.controller.animateToPage(widget.controller.page!.toInt() + 1,
                       duration: Duration(milliseconds: 400),
                       curve: Curves.easeIn
                   );
