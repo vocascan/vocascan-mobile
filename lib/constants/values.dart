a41b0ae7927225bf84bb87267e28b84e550f91d6
Florian Sabonchi <sabonchi@posteo.de>
2021-08-27 13:21:32 +0200

    values.dart added this file contains values that are used generally

---
+++ lib/constants/values.dart
@@ -0,0 +1,3 @@
+const emailRegex = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+const urlRegex =  r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)";
+const httpRegex = r"http[s]?://";
