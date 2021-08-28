c091c7ed8a625613ac805f593a9bfc1da87269ee
Florian Sabonchi <sabonchi@posteo.de>
2021-08-28 22:23:39 +0200

    api client updated

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -1,12 +1,7 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 import 'package:simple_json_mapper/simple_json_mapper.dart';
-
-class ApiResponse {
-  final Response response;
-  final Map data;
-
-  const ApiResponse(this.response, this.data);
-}
 
 class ApiClientService<T>{
   String homeServer = "";
@@ -28,6 +23,20 @@ class ApiClientService<T>{
    Future<T?> endpointGet<T>(String endpoint) async {
     Uri url = Uri.parse(this.homeServer + endpoint);
     Response response = await get(url);
+
+    final result = JsonMapper.deserialize<T>(response.body);
+
+    return result;
+  }
+
+  Future<T?> endpointPost<T>(String endpoint, Map data) async {
+    Map<String, String> headers = {
+      "accept": "application/json",
+      "Content-Type": "application/json"
+    };
+
+    Uri url = Uri.parse(this.homeServer + endpoint);
+    Response response = await post(url,headers: headers,  body: utf8.encode(json.encode(data)));
 
     final result = JsonMapper.deserialize<T>(response.body);
 
