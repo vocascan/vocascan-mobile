9b596bf9425f350ce4b52774b0d952a076e2d2d3
Florian Sabonchi <sabonchi@posteo.de>
2021-08-29 22:00:04 +0200

    updated auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:vocascan_mobile/api/schemas/endpoint_login.dart';
+import 'package:vocascan_mobile/api/schemas/endpoint_register.dart';
+
+import 'api_client.dart';
 
 class AuthService {
   // static instance
   static AuthService? instance;
+  static ApiClientService _apiClientService = ApiClientService.getInstance();
 
   // will never be null, but the key is generated
   // in the main.dart after class init
@@ -21,11 +26,27 @@ class AuthService {
     return false;
   }
 
-  Future<bool> loginUser(mail, password) async {
-    return false;
+  Future<bool> loginUser(email, password) async {
+    Map data = {
+      "email": email,
+      "password": password,
+    };
+
+    EndpointLogin? result = await _apiClientService.endpointPost<EndpointLogin>("user/login", data);
+
+    return result != null;
   }
 
-  Future<bool> signupUser(username, mail, password) async {
-    return false;
+  Future<bool> signupUser(username, email, password) async {
+
+    Map data = {
+      "email": email,
+      "password": password,
+      "username": username
+    };
+
+    EndpointRegister? result = await _apiClientService.endpointPost<EndpointRegister>("user/register", data);
+
+    return result != null;
   }
 }
