ed3c6f39e19fba59d818892c5f368e8e76b4ac34
Florian Sabonchi <sabonchi@posteo.de>
2021-09-15 22:04:36 +0200

    UserAccountsDrawerHeader added

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,8 +5,32 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:vocascan_mobile/constants/colors.dart';
 import 'package:vocascan_mobile/pages/widgets/add_vocabulary.dart';
+import 'package:vocascan_mobile/services/storage.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  StorageService _storageService = StorageService.getInstance();
+  String _email = "";
+  String _username = "";
+
+  @override
+  initState() {
+    super.initState();
+
+    setState(() {
+      _initState();
+    });
+  }
+
+  _initState() async {
+    _email = (await _storageService.get("email"))!;
+    _username = (await _storageService.get("username"))!;
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -17,40 +41,52 @@ class HomePage extends StatelessWidget{
         leading: Icon(Icons.menu),
       ),
       body: Center(
-        child: SizedBox(
-          height: size.height * 0.25,
-          width: size.height * 0.25,
-          child: SvgPicture.asset(
-            "assets/images/illustrations/undraw_empty.svg",
-          ))
-      ),
+          child: SizedBox(
+              height: size.height * 0.25,
+              width: size.height * 0.25,
+              child: SvgPicture.asset(
+                "assets/images/illustrations/undraw_empty.svg",
+              ))),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
             context: context,
-            builder: (BuildContext context) => AddVocabularyPopup(
-
-            ),
+            builder: (BuildContext context) => AddVocabularyPopup(),
           );
         },
         child: Icon(Icons.add),
-        backgroundColor:  primary,
+        backgroundColor: primary,
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              child: Row(
-
-              )
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.white),
+              accountEmail: Text(
+                _email,
+                style: TextStyle(color: Colors.black),
+              ),
+              accountName: Text(
+                _username,
+                style: TextStyle(color: Colors.black),
+              ),
+              currentAccountPicture: CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.black,
+                child: CircleAvatar(
+                    radius: 35,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.black,
+                    )),
+              ),
             ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text(tr("settings")),
-              onTap: () {
-
-              },
+              onTap: () {},
             ),
           ],
         ),
