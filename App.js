873c1e584863209d11a4295d8790645dc113dd1c
Tracer1337 <merlin.moelter@gmail.com>
2022-05-26 16:02:46 +0200

    feat: use safe-area view

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import { SafeAreaView, StyleSheet, Platform } from 'react-native';
 import WebView from 'react-native-webview';
 
 const uri = __DEV__
-  ? 'http://10.0.2.2:3000'
+  ? Platform.select({
+      android: 'http://10.0.2.2:3000',
+      default: 'http://localhost:3000',
+    })
   : 'file:///android_asset/build/index.html';
 
 const App = () => {
   return (
-    <WebView
-      source={{ uri }}
-      allowFileAccess
-      setBuiltInZoomControls={false}
-      bounces={false}
-    />
+    <SafeAreaView style={styles.safeArea}>
+      <WebView
+        source={{ uri }}
+        allowFileAccess
+        setBuiltInZoomControls={false}
+        bounces={false}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
 
 export default App;
